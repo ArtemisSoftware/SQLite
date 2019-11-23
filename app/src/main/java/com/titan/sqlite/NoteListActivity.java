10ce9ee678cb4a7ca0ac907608ce74d4ad256503
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-23 10:47:30 +0000

    Setting Up a RecyclerView

--- app/src/main/java/com/titan/sqlite/NoteListActivity.java
+++ app/src/main/java/com/titan/sqlite/NoteListActivity.java
@@ -1,14 +1,50 @@
 package com.titan.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.titan.sqlite.adapters.NotesRecyclerAdapter;
+import com.titan.sqlite.models.Note;
+
+import java.util.ArrayList;
+
 public class NoteListActivity extends AppCompatActivity {
+
+
+    private RecyclerView recyclerView;
+
+    private ArrayList<Note> notes = new ArrayList<>();
+    private NotesRecyclerAdapter notesRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_list);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        initRecyclerView();
+        insertFakeNotes();
+    }
+
+    private void insertFakeNotes(){
+        for(int i = 0; i < 1000; i++){
+            Note note = new Note();
+            note.setTitle("title #" + i);
+            note.setContent("content #: " + i);
+            note.setTimestamp("Jan 2019");
+            notes.add(note);
+        }
+        notesRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private void initRecyclerView(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        notesRecyclerAdapter = new NotesRecyclerAdapter(notes);
+        recyclerView.setAdapter(notesRecyclerAdapter);
     }
 }
