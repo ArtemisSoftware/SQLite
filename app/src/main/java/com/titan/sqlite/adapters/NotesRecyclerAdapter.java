aca48589b441a1716a38403751cf388bd4ac99f7
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-23 10:37:01 +0000

    RecyclerView Adapter

--- app/src/main/java/com/titan/sqlite/adapters/NotesRecyclerAdapter.java
+++ app/src/main/java/com/titan/sqlite/adapters/NotesRecyclerAdapter.java
@@ -1,4 +1,55 @@
 package com.titan.sqlite.adapters;
 
-public class NotesRecyclerAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.titan.sqlite.R;
+import com.titan.sqlite.models.Note;
+
+import java.util.ArrayList;
+
+public class NotesRecyclerAdapter extends RecyclerView.Adapter<NotesRecyclerAdapter.ViewHolder>{
+
+    private ArrayList<Note> notes = new ArrayList<>();
+
+    public NotesRecyclerAdapter(ArrayList<Note> notes) {
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_note_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.timestamp.setText(notes.get(position).getTimestamp());
+        holder.title.setText(notes.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView title, timestamp;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.note_title);
+            timestamp = itemView.findViewById(R.id.note_timestamp);
+        }
+    }
 }
