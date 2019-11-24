f58172dc39f9877964189052aed17ef6a179bf20
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-24 19:47:14 +0000

    NoteActivity Initialization

--- app/src/main/java/com/titan/sqlite/NoteActivity.java
+++ app/src/main/java/com/titan/sqlite/NoteActivity.java
@@ -3,18 +3,44 @@ package com.titan.sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.titan.sqlite.models.Note;
 
 public class NoteActivity extends AppCompatActivity {
+
+    private LinedEditText linedEditText;
+    private EditText editTitle;
+    private TextView txtTitle;
+
+    private boolean isNewNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
 
-        if(getIntent().hasExtra("selected_note")) {
+        linedEditText = findViewById(R.id.note_text);
+        editTitle = findViewById(R.id.note_edit_title);
+        txtTitle = findViewById(R.id.note_text_title);
+
+        if(getIncomingIntent()) {
+            //EDIT MODE
             Note note = getIntent().getParcelableExtra("selected_note");
         }
+        else{
+            //VIEW MODE
+        }
+    }
+
+    private boolean getIncomingIntent(){
+        if(getIntent().hasExtra("selected_note")) {
+            isNewNote = false;
+            return false;
+        }
+
+        isNewNote = true;
+        return true;
     }
 }
