cc89da9f80c045686fd6ed69cbaeeb8fc14e0463
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-25 20:13:26 +0000

    Edit Mode and View Mode

--- app/src/main/java/com/titan/sqlite/NoteActivity.java
+++ app/src/main/java/com/titan/sqlite/NoteActivity.java
@@ -7,6 +7,8 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.titan.sqlite.models.Note;
@@ -16,11 +18,18 @@ public class NoteActivity extends AppCompatActivity implements View.OnTouchListe
     private LinedEditText linedEditText;
     private EditText editTitle;
     private TextView txtTitle;
+    private RelativeLayout checkContainer, backArrowContainer;
+    private ImageButton check, backArrow;
 
     private boolean isNewNote;
     private Note note;
 
     private GestureDetector gestureDetector;
+
+    private static final int EDIT_MODE_ENABLED = 1;
+    private static final int EDIT_MODE_DISABLED = 0;
+    private int mode;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +39,15 @@ public class NoteActivity extends AppCompatActivity implements View.OnTouchListe
         linedEditText = findViewById(R.id.note_text);
         editTitle = findViewById(R.id.note_edit_title);
         txtTitle = findViewById(R.id.note_text_title);
+        checkContainer = findViewById(R.id.check_container);
+        backArrowContainer = findViewById(R.id.back_arrow_container);
+        check = findViewById(R.id.toolbar_check);
+        backArrow = findViewById(R.id.toolbar_back_arrow);
 
         if(getIncomingIntent()) {
             //EDIT MODE
             setNewNoteProperties();
+            enabledEditMode();
         }
         else{
             //VIEW MODE
@@ -41,6 +55,26 @@ public class NoteActivity extends AppCompatActivity implements View.OnTouchListe
         }
 
         setListeners();
+    }
+
+    private void enabledEditMode(){
+        backArrowContainer.setVisibility(View.GONE);
+        checkContainer.setVisibility(View.VISIBLE);
+
+        txtTitle.setVisibility(View.GONE);
+        editTitle.setVisibility(View.VISIBLE);
+
+        mode = EDIT_MODE_ENABLED;
+    }
+
+    private void disableEditMode(){
+        backArrowContainer.setVisibility(View.VISIBLE);
+        checkContainer.setVisibility(View.GONE);
+
+        txtTitle.setVisibility(View.VISIBLE);
+        editTitle.setVisibility(View.GONE);
+
+        mode = EDIT_MODE_DISABLED;
     }
 
 
@@ -52,10 +86,12 @@ public class NoteActivity extends AppCompatActivity implements View.OnTouchListe
     private boolean getIncomingIntent(){
         if(getIntent().hasExtra("selected_note")) {
             note = getIntent().getParcelableExtra("selected_note");
+            mode = EDIT_MODE_DISABLED;
             isNewNote = false;
             return false;
         }
 
+        mode = EDIT_MODE_ENABLED;
         isNewNote = true;
         return true;
     }
@@ -79,6 +115,12 @@ public class NoteActivity extends AppCompatActivity implements View.OnTouchListe
     }
 
     @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        enabledEditMode();
+        return false;
+    }
+
+    @Override
     public boolean onDoubleTapEvent(MotionEvent e) {
         return false;
     }
@@ -89,10 +131,7 @@ public class NoteActivity extends AppCompatActivity implements View.OnTouchListe
         return false;
     }
 
-    @Override
-    public boolean onDoubleTap(MotionEvent e) {
-        return false;
-    }
+
 
     @Override
     public boolean onDown(MotionEvent e) {
